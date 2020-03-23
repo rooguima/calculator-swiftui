7974432d86c549c8786d59061166628014efdc8f
Rodrigo Guimarães <rooguima@icloud.com>
2020-03-22 21:04:04 -0300

    Initial Commit

---
+++ calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by Rodrigo Guimarães on 22/03/20.
+//  Copyright © 2020 Rodrigo Guimarães. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
